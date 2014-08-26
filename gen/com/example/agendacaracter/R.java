667be94806bb2b0d197aaa4b5c5bcc0ce196a8d7
Cinthya <avilasauna@gmail.com>
2014-08-26 13:02:02 -0500

    fin_login

--- gen/com/example/agendacaracter/R.java
+++ gen/com/example/agendacaracter/R.java
@@ -35,25 +35,27 @@ public final class R {
         public static final int right_cab=0x7f040001;
     }
     public static final class drawable {
-        public static final int facebook35=0x7f020000;
-        public static final int ic_action_overflow=0x7f020001;
-        public static final int ic_action_user=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int key135=0x7f020004;
-        public static final int list38=0x7f020005;
-        public static final int overflow=0x7f020006;
+        public static final int facebook1=0x7f020000;
+        public static final int facebook33=0x7f020001;
+        public static final int gmail=0x7f020002;
+        public static final int ic_action_overflow=0x7f020003;
+        public static final int ic_action_user=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int key135=0x7f020006;
+        public static final int list38=0x7f020007;
+        public static final int overflow=0x7f020008;
+        public static final int twitter=0x7f020009;
     }
     public static final class id {
-        public static final int ImageButton01=0x7f090009;
-        public static final int ImageButton02=0x7f090008;
-        public static final int action_listar=0x7f09000d;
-        public static final int action_menu=0x7f09000c;
-        public static final int action_preguntas=0x7f09000e;
-        public static final int action_settings=0x7f09000b;
+        public static final int action_listar=0x7f09000c;
+        public static final int action_menu=0x7f09000b;
+        public static final int action_preguntas=0x7f09000d;
+        public static final int action_settings=0x7f09000a;
         public static final int btnCrearcuenta=0x7f090007;
         public static final int btnRegister=0x7f090006;
-        public static final int imageButton1=0x7f09000a;
         public static final int imageView1=0x7f090002;
+        public static final int imageView2=0x7f090008;
+        public static final int imageView3=0x7f090009;
         public static final int listView1=0x7f090003;
         public static final int relativeLayout1=0x7f090001;
         public static final int textView1=0x7f090000;
