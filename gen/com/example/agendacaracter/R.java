82b6108f9f9e058a2856b9f44c637a8fd700f4de
dreyesgutierrez <darg18diego@gmail.com>
2014-08-28 17:01:26 -0500

    Proyecto Corregido

--- gen/com/example/agendacaracter/R.java
+++ gen/com/example/agendacaracter/R.java
@@ -71,24 +71,17 @@ public final class R {
     }
     public static final class id {
         public static final int EditText01=0x7f090006;
-<<<<<<< Updated upstream
         public static final int RadioButton01=0x7f090016;
         public static final int RadioButton02=0x7f090015;
         public static final int RadioButton03=0x7f090018;
         public static final int RadioButton04=0x7f090017;
         public static final int RadioButton05=0x7f090019;
         public static final int RadioGroup01=0x7f090014;
-        public static final int action_listar=0x7f09002b;
-        public static final int action_menu=0x7f09002a;
-        public static final int action_preguntas=0x7f09002c;
+        public static final int action_cerrar_sesion=0x7f09002b;
+        public static final int action_inicio=0x7f09002a;
+        public static final int action_listar=0x7f09002d;
+        public static final int action_menu=0x7f09002c;
         public static final int action_settings=0x7f090029;
-=======
-        public static final int action_cerrar_sesion=0x7f09001f;
-        public static final int action_inicio=0x7f09001e;
-        public static final int action_listar=0x7f090021;
-        public static final int action_menu=0x7f090020;
-        public static final int action_settings=0x7f09001d;
->>>>>>> Stashed changes
         public static final int btnCrearcuenta=0x7f090008;
         public static final int btnRegister=0x7f09001b;
         public static final int button1=0x7f090003;
@@ -98,10 +91,8 @@ public final class R {
         public static final int imageView2=0x7f09000c;
         public static final int imageView3=0x7f09000d;
         public static final int imageView4=0x7f09000b;
-<<<<<<< Updated upstream
         public static final int listView1=0x7f09001a;
-        public static final int opcVerLibros=0x7f09002d;
-        public static final int opcVerLogin=0x7f09002e;
+        public static final int opcVerLibros=0x7f09002e;
         public static final int radio0=0x7f09000f;
         public static final int radio1=0x7f090010;
         public static final int radio2=0x7f090011;
@@ -112,14 +103,6 @@ public final class R {
         public static final int tableRow2=0x7f09001d;
         public static final int tableRow3=0x7f09001e;
         public static final int tableRow4=0x7f090020;
-=======
-        public static final int listView1=0x7f09000e;
-        public static final int opcVerLibros=0x7f090022;
-        public static final int tableRow1=0x7f090010;
-        public static final int tableRow2=0x7f090011;
-        public static final int tableRow3=0x7f090012;
-        public static final int tableRow4=0x7f090014;
->>>>>>> Stashed changes
         public static final int textView1=0x7f090000;
         public static final int textView2=0x7f090009;
         public static final int textView3=0x7f09000a;
@@ -131,15 +114,9 @@ public final class R {
         public static final int textView9=0x7f090023;
         public static final int txtPass=0x7f090007;
         public static final int txtUserName=0x7f090005;
-<<<<<<< Updated upstream
         public static final int txtcualidad=0x7f090026;
         public static final int txtid=0x7f090028;
         public static final int txtmes=0x7f090027;
-=======
-        public static final int txtcualidad=0x7f09001a;
-        public static final int txtid=0x7f09001c;
-        public static final int txtmes=0x7f09001b;
->>>>>>> Stashed changes
     }
     public static final class layout {
         public static final int activity_codigo__reserva=0x7f030000;
@@ -159,7 +136,6 @@ public final class R {
         public static final int crear_cuenta=0x7f080001;
         public static final int demito=0x7f080002;
         public static final int descripcion__libros=0x7f080003;
-<<<<<<< Updated upstream
         public static final int evaluacion_diaria1=0x7f080004;
         public static final int evaluacion_diaria2=0x7f080005;
         public static final int lista_cualidades=0x7f080006;
@@ -168,51 +144,21 @@ public final class R {
         public static final int menu_contextual_cualidades=0x7f080009;
         public static final int pensamiento=0x7f08000a;
         public static final int referencia=0x7f08000b;
-=======
-        public static final int lista_cualidades=0x7f080004;
-        public static final int login=0x7f080005;
-        public static final int main=0x7f080006;
-        public static final int menu_contextual_cualidades=0x7f080007;
-        public static final int pensamiento=0x7f080008;
-        public static final int referencia=0x7f080009;
->>>>>>> Stashed changes
     }
     public static final class string {
         public static final int Fuente=0x7f050009;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-<<<<<<< Updated upstream
-        public static final int codigo__reserva=0x7f050015;
-        public static final int contrasena=0x7f05000c;
-        public static final int correoelectronico=0x7f05000d;
-        public static final int descripcion__libros=0x7f050013;
-        public static final int evaluacion_diaria=0x7f050016;
-        public static final int hello_world=0x7f050001;
-        public static final int lista_cualidades=0x7f050003;
-        public static final int msg_MostrarCualidad=0x7f050004;
-        public static final int msg_encuesta_diaria=0x7f050009;
-        public static final int msg_lista_cualidades=0x7f050008;
-        public static final int pensamiento=0x7f05000f;
-        public static final int referencia=0x7f050011;
-        public static final int title_activity_codigo__reserva=0x7f050014;
-        public static final int title_activity_crear_cuenta=0x7f05000a;
-        public static final int title_activity_demito=0x7f05000b;
-        public static final int title_activity_descripcion__libros=0x7f050012;
-        public static final int title_activity_evaluacion_diaria1=0x7f050017;
-        public static final int title_activity_evaluacion_diaria2=0x7f050018;
-        public static final int title_activity_lista_cualidades=0x7f050006;
-        public static final int title_activity_login=0x7f050007;
-        public static final int title_activity_pensamiento=0x7f05000e;
-        public static final int title_activity_referencia=0x7f050010;
-=======
         public static final int codigo__reserva=0x7f050017;
         public static final int contrasena=0x7f05000e;
         public static final int correoelectronico=0x7f05000f;
         public static final int descripcion__libros=0x7f050015;
+        public static final int evaluacion_diaria=0x7f050018;
         public static final int hello_world=0x7f050001;
         public static final int lista_cualidades=0x7f050003;
         public static final int msg_MostrarCualidad=0x7f050004;
         public static final int msg_cerrar_sesion=0x7f050006;
+        public static final int msg_encuesta_diaria=0x7f05000c;
         /**  Menus Listado  
          */
         public static final int msg_inicio=0x7f050007;
@@ -223,14 +169,15 @@ public final class R {
         public static final int pensamiento=0x7f050011;
         public static final int referencia=0x7f050013;
         public static final int title_activity_codigo__reserva=0x7f050016;
-        public static final int title_activity_crear_cuenta=0x7f05000c;
+        public static final int title_activity_crear_cuenta=0x7f05001b;
         public static final int title_activity_demito=0x7f05000d;
         public static final int title_activity_descripcion__libros=0x7f050014;
+        public static final int title_activity_evaluacion_diaria1=0x7f050019;
+        public static final int title_activity_evaluacion_diaria2=0x7f05001a;
         public static final int title_activity_lista_cualidades=0x7f05000a;
         public static final int title_activity_login=0x7f05000b;
         public static final int title_activity_pensamiento=0x7f050010;
         public static final int title_activity_referencia=0x7f050012;
->>>>>>> Stashed changes
     }
     public static final class style {
         /** 
