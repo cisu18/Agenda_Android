5e560ffce0ec6c770c8a5a647f2bdf963d8d46e3
mroque <mpc4mirl@gmail.com>
2014-09-02 11:42:30 -0500

    Validaciones

--- src/com/example/agendacaracter/Pensamiento.java
+++ src/com/example/agendacaracter/Pensamiento.java
@@ -1,8 +1,27 @@
 package com.example.agendacaracter;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.example.entidad.Cualidades;
+import com.example.reutilizables.AdaptadorCualidades;
+
 import android.app.Activity;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -29,7 +48,7 @@ public class Pensamiento extends Activity {
 		txtAutor.setTypeface(miPropiaTypeFace);
 
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -48,4 +67,69 @@ public class Pensamiento extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	/*private class ReadJSONFeedTask extends
+			AsyncTask<String, Void, String> {
+
+		protected String doInBackground(String... urls) {
+			return readJSONFeed(urls[0]);
+		}
+
+		protected void onPostExecute(String result) {
+			try {
+				JSONArray jsonArray = new JSONArray(result);
+				JSONObject datos = new JSONObject();
+				cualidades = new ArrayList<Cualidades>();
+
+				for (int i = 0; i < jsonArray.length(); i++) {
+					Cualidades c = new Cualidades();
+					datos = jsonArray.getJSONObject(i);
+					c.setId(datos.getString("id"));
+					c.setCualidad(datos.getString("cualidad"));
+					c.setMes(datos.getString("mes"));
+					cualidades.add(c);
+
+				}
+
+				lista_mensual.setAdapter(new AdaptadorCualidades(
+						getApplicationContext(), cualidades));
+				// adaptadorlista=new
+				// ArrayAdapter<Cualidades>(getApplicationContext(),
+				// android.R.layout.simple_list_item_1, cualidades);
+				// lista_mensual.setAdapter(adaptadorlista);
+
+			} catch (Exception e) {
+				Log.d("ReadCualidadesJSONFeedTask", e.getLocalizedMessage());
+			}
+		}
+	}
+
+	public String readJSONFeed(String URL) {
+		StringBuilder stringBuilder = new StringBuilder();
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(URL);
+		try {
+			HttpResponse response = httpClient.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream inputStream = entity.getContent();
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(inputStream));
+				String line;
+
+				while ((line = reader.readLine()) != null) {
+					stringBuilder.append(line);
+				}
+				inputStream.close();
+			} else {
+				Log.d("JSON", "No se ha podido descargar archivo");
+			}
+		} catch (Exception e) {
+			Log.d("readJSONFeed", e.getLocalizedMessage());
+		}
+		return stringBuilder.toString();
+	}*/
 }
---
+++ src/com/example/entidad/Diario.java
@@ -0,0 +1,70 @@
+package com.example.entidad;
+
+public class Diario {
+	private String id;
+	private String fecha;
+	private String mes;
+	private String numeroVersiculo;
+	private String versiculo;
+	private String planLectura;
+	private String autorPensamiento;
+	private String pensamiento;
+	private String idCualidad;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getMes() {
+		return mes;
+	}
+	public void setMes(String mes) {
+		this.mes = mes;
+	}
+	public String getNumeroVersiculo() {
+		return numeroVersiculo;
+	}
+	public void setNumeroVersiculo(String numeroVersiculo) {
+		this.numeroVersiculo = numeroVersiculo;
+	}
+	public String getVersiculo() {
+		return versiculo;
+	}
+	public void setVersiculo(String versiculo) {
+		this.versiculo = versiculo;
+	}
+	public String getPlanLectura() {
+		return planLectura;
+	}
+	public void setPlanLectura(String planLectura) {
+		this.planLectura = planLectura;
+	}
+	public String getAutorPensamiento() {
+		return autorPensamiento;
+	}
+	public void setAutorPensamiento(String autorPensamiento) {
+		this.autorPensamiento = autorPensamiento;
+	}
+	public String getPensamiento() {
+		return pensamiento;
+	}
+	public void setPensamiento(String pensamiento) {
+		this.pensamiento = pensamiento;
+	}
+	public String getIdCualidad() {
+		return idCualidad;
+	}
+	public void setIdCualidad(String idCualidad) {
+		this.idCualidad = idCualidad;
+	}
+	
+	
+}
