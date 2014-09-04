d170aa267e70b72ea9bf467b8064927507325256
mroque <mpc4mirl@gmail.com>
2014-09-04 13:06:16 -0500

    Programación android

--- src/com/example/agendacaracter/Evaluacion_diaria2.java
+++ src/com/example/agendacaracter/Evaluacion_diaria2.java
@@ -1,5 +1,21 @@
 package com.example.agendacaracter;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -8,7 +24,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,10 +47,7 @@ public class Evaluacion_diaria2 extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_evaluacion_diaria2);
 		
-		
-		
 		Typeface miPropiaTypeFace = Typeface.createFromAsset(getAssets(),"fonts/Myriad_Pro.ttf");
-		
 
         TextView title = (TextView)findViewById(R.id.txt_cabecera);
 
@@ -61,24 +76,38 @@ public class Evaluacion_diaria2 extends Activity implements OnClickListener {
 			Bundle b = getIntent().getExtras();
 			puntaje=b.getInt("puntaje");			
 			puntaje =puntaje+Integer.parseInt(rdbPregunta03.getText().toString());
-		
-			alert = new AlertDialog.Builder(Evaluacion_diaria2.this).create();
-			alert.setTitle("Resultado");
-			//alert.setIcon(R.drawable.ic_action_accept);
-			alert.setMessage("Su puntaje es "+puntaje);
-			alert.setButton("Aceptar",
-					new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog,
-								int which) {
-							alert.hide();
-						}
-					});
-			alert.show();
+			puntaje = Math.round(puntaje/3);			
+			String fecha = getFechaActual();
+			String usuario = "1";
+			String cualidad = "1"; 
+			new ReadJSONFeedTask()
+			.execute("http://192.168.0.55/Agenda_WS/puntaje_cualidad/puntaje/fecha/"+fecha+"/usuario/"+usuario+"/puntaje/"+puntaje+"/cualidad/"+cualidad+"/format/json");
 			
 			//Intent i = new Intent(this, Evaluacion_diaria2.class);
 			//startActivity(i);
 			break;
 		}
+	}
+	
+	public static String getFechaActual() {
+		Date ahora = new Date();
+		SimpleDateFormat formateador = new SimpleDateFormat("dd-MM-yyyy");
+		return formateador.format(ahora);
+	}
+	
+	public void mostrarAlerta(int puntaje, int mensual){
+		alert = new AlertDialog.Builder(Evaluacion_diaria2.this).create();
+		alert.setTitle("Resultado");
+		//alert.setIcon(R.drawable.ic_action_accept);
+		alert.setMessage("Su puntaje del día de hoy es "+puntaje +"\n"+"Su puntaje mensual es "+mensual);
+		alert.setButton("Aceptar",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,
+							int which) {						
+						alert.hide();
+					}
+				});
+		alert.show();
 	}
 
 	@Override
@@ -98,5 +127,55 @@ public class Evaluacion_diaria2 extends Activity implements OnClickListener {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private class ReadJSONFeedTask extends AsyncTask<String, Void, String> {
+
+		protected String doInBackground(String... urls) {
+			return readJSONFeed(urls[0]);
+		}
+
+		protected void onPostExecute(String result) {
+			try {
+				JSONArray jsonArray = new JSONArray(result);
+				JSONObject datos = new JSONObject();
+				datos = jsonArray.getJSONObject(0);
+				
+				mostrarAlerta(puntaje,Integer.parseInt(datos.getString("puntaje")));
+
+			} catch (Exception e) {
+				Log.e("onPostExecute", e.getLocalizedMessage());
+			}
+		}
+	}
+
+	public String readJSONFeed(String URL) {
+		StringBuilder stringBuilder = new StringBuilder();
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(URL);
+		try {
+			HttpResponse response = httpClient.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream inputStream = entity.getContent();
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(inputStream));
+				String line;
+
+				while ((line = reader.readLine()) != null) {
+					stringBuilder.append(line);
+				}
+				inputStream.close();
+			} else {
+				Log.e("JSON", "No se ha podido descargar archivo");
+			}
+		} catch (Exception e) {
+			Log.e("readJSONFeed", e.getLocalizedMessage());
+		}
+
+		return stringBuilder.toString();
 	}
 }
