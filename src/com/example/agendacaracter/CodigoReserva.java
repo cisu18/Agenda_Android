5aefb17cd21fa9ef7ed6918b1b534abae241dba4
Cinthya <avilasauna@gmail.com>
2014-09-26 12:56:31 -0500

    reserva

--- src/com/example/agendacaracter/CodigoReserva.java
+++ src/com/example/agendacaracter/CodigoReserva.java
@@ -14,7 +14,7 @@ public class CodigoReserva extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-<<<<<<< HEAD
+
 		setContentView(R.layout.activity_codigo_reserva);	
 		
 		Typeface tfHelveticaCond = Typeface.createFromAsset(getAssets(),
@@ -32,30 +32,12 @@ public class CodigoReserva extends Activity {
 		Button aceptar = (Button)findViewById(R.id.btn_reserva_aqui);
 		aceptar.setTypeface(HelveticaBoldTypeFace);
 		
-		
-=======
-		setContentView(R.layout.activity_codigo_reserva);
-
-		Typeface miPropiaTypeFace = Typeface.createFromAsset(getAssets(),
-				"fonts/Myriad_Pro.ttf");
-
-		TextView codigoreserva = (TextView) findViewById(R.id.txv_cabecera_descripcion);
-
-		codigoreserva.setTypeface(miPropiaTypeFace);
-
-		TextView indicaciones = (TextView) findViewById(R.id.txv_descripcion_libro);
-		indicaciones.setTypeface(miPropiaTypeFace);
-
-		Button aceptar = (Button) findViewById(R.id.btn_reserva_aqui);
-		aceptar.setTypeface(miPropiaTypeFace);
-
 		Bundle bundle = getIntent().getExtras();
 		Toast.makeText(
 				getApplicationContext(),
 				bundle.getString("idMultimedia")
 						+ bundle.getString("idUsuario"), Toast.LENGTH_SHORT)
 				.show();
->>>>>>> 5d9a9fff198101473f82e767906aa1eb9c47dfd8
 	}
 
 	@Override
