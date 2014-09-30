9312edee8aa85a54f3a2a07b9026815c8a388caf
mroque <mpc4mirl@gmail.com>
2014-09-30 15:49:45 -0500

    Face final

--- src/com/example/agendacaracter/Login.java
+++ src/com/example/agendacaracter/Login.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -18,6 +19,7 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -25,23 +27,54 @@ import android.content.SharedPreferences.Editor;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.reutilizables.Util;
+import com.facebook.AppEventsLogger;
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.FacebookOperationCanceledException;
+import com.facebook.FacebookRequestError;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphObject;
+import com.facebook.model.GraphPlace;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.FacebookDialog;
+import com.facebook.widget.FriendPickerFragment;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.PickerFragment;
+import com.facebook.widget.PlacePickerFragment;
 
 public class Login extends Activity implements OnClickListener {
 
 	public EditText etxUsuarioNombre;
 	public EditText etxContrasenia;
 	public TextView txvBienvenido;
+	
+	private final String PENDING_ACTION_BUNDLE_KEY = "com.facebook.samples.hellofacebook:PendingAction";
+
+	private LoginButton loginButton;
+	private PendingAction pendingAction = PendingAction.NONE;
+	private ViewGroup controlsContainer;
+	private GraphUser user;
+	private GraphPlace place;
+	private List<GraphUser> tags;
+	private boolean canPresentShareDialog;
+	@SuppressWarnings("unused")
+	private boolean canPresentShareDialogWithPhotos;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -79,8 +112,55 @@ public class Login extends Activity implements OnClickListener {
 		ImageView img_twitter = (ImageView) findViewById(R.id.imv_twitter_descripcion);
 		img_twitter.setOnClickListener(this);
 
-		ImageView img_faccebook = (ImageView) findViewById(R.id.imv_login_facebook);
-		img_faccebook.setOnClickListener(this);
+		
+		
+		uiHelper = new UiLifecycleHelper(this, callback);
+		uiHelper.onCreate(savedInstanceState);
+
+		if (savedInstanceState != null) {
+			String name = savedInstanceState
+					.getString(PENDING_ACTION_BUNDLE_KEY);
+			pendingAction = PendingAction.valueOf(name);
+		}
+		
+		loginButton = (LoginButton) findViewById(R.id.login_button);
+		loginButton.setBackgroundResource(R.drawable.face);
+		loginButton.setCompoundDrawablesWithIntrinsicBounds(0,0,0,0);
+		loginButton
+				.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+					@Override
+					public void onUserInfoFetched(GraphUser user) {
+						Login.this.user = user;
+						updateUI();						
+						handlePendingAction();
+					}
+				});
+		
+		controlsContainer = (ViewGroup) findViewById(R.id.main_ui_container);
+
+		/*final FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+		if (fragment != null) {			
+			controlsContainer.setVisibility(View.GONE);
+			if (fragment instanceof FriendPickerFragment) {
+				setFriendPickerListeners((FriendPickerFragment) fragment);
+			} else if (fragment instanceof PlacePickerFragment) {
+				setPlacePickerListeners((PlacePickerFragment) fragment);
+			}
+		}		
+		fm.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+			@Override
+			public void onBackStackChanged() {
+				if (fm.getBackStackEntryCount() == 0) {					
+					controlsContainer.setVisibility(View.VISIBLE);
+				}
+			}
+		});*/
+		
+		canPresentShareDialog = FacebookDialog.canPresentShareDialog(this,
+				FacebookDialog.ShareDialogFeature.SHARE_DIALOG);		
+		canPresentShareDialogWithPhotos = FacebookDialog.canPresentShareDialog(
+				this, FacebookDialog.ShareDialogFeature.PHOTOS);
 
 	}
 
@@ -138,11 +218,7 @@ public class Login extends Activity implements OnClickListener {
 		case R.id.imv_twitter_descripcion:
 			i = new Intent(this, TwitterActivity.class);
 			startActivity(i);
-			break;
-		case R.id.imv_login_facebook:
-			i = new Intent(this, FacebookActivity.class);
-			startActivity(i);
-			break;
+			break;		
 		default:
 
 			break;
@@ -181,6 +257,7 @@ public class Login extends Activity implements OnClickListener {
 				editor.putString("username", datos.getString("usuario"));
 				editor.putString("useremail", datos.getString("email"));
 				editor.commit();
+				Session.getActiveSession().closeAndClearTokenInformation();
 				Intent in = new Intent(getApplicationContext(),
 						MainActivity.class);
 				startActivity(in);
@@ -239,5 +316,268 @@ public class Login extends Activity implements OnClickListener {
 		}
 
 		return stringBuilder.toString();
+	}
+	
+
+	private enum PendingAction {
+		NONE, POST_PHOTO, POST_STATUS_UPDATE
+	}
+
+	private UiLifecycleHelper uiHelper;
+
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		@Override
+		public void call(Session session, SessionState state,
+				Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
+	};
+
+	private FacebookDialog.Callback dialogCallback = new FacebookDialog.Callback() {
+		@Override
+		public void onError(FacebookDialog.PendingCall pendingCall,
+				Exception error, Bundle data) {
+			Log.d("HelloFacebook", String.format("Error: %s", error.toString()));
+		}
+
+		@Override
+		public void onComplete(FacebookDialog.PendingCall pendingCall,
+				Bundle data) {
+			Log.d("HelloFacebook", "Success!");
+		}
+	};
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		uiHelper.onResume();	
+		AppEventsLogger.activateApp(this);
+		updateUI();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		uiHelper.onSaveInstanceState(outState);
+		outState.putString(PENDING_ACTION_BUNDLE_KEY, pendingAction.name());
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		uiHelper.onActivityResult(requestCode, resultCode, data, dialogCallback);
+
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		uiHelper.onPause();
+		AppEventsLogger.deactivateApp(this);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+
+	private void onSessionStateChange(Session session, SessionState state,
+			Exception exception) {
+		if (pendingAction != PendingAction.NONE
+				&& (exception instanceof FacebookOperationCanceledException || exception instanceof FacebookAuthorizationException)) {
+			new AlertDialog.Builder(Login.this)
+					.setTitle(R.string.cancelled)
+					.setMessage(R.string.permission_not_granted)
+					.setPositiveButton(R.string.ok, null).show();
+			pendingAction = PendingAction.NONE;
+		} else if (state == SessionState.OPENED_TOKEN_UPDATED) {
+			handlePendingAction();
+		}
+		updateUI();
+
+	}
+
+	private void updateUI() {
+		Session session = Session.getActiveSession();
+		boolean enableButtons = (session != null && session.isOpened());
+		if (enableButtons && user != null) {			
+			String url = getResources().getString(
+					R.string.url_web_service)
+					+ "users/create_user_social/format/json";
+			String username="fb"+user.getId();
+			String password=user.getId()+"fb";
+			String email=user.getId();
+			String firstname=user.getFirstName();		
+			String lastname=user.getLastName();
+			String ip="no ip";					
+			new RegistroUsuarioJSONFeedTask()
+					.execute(url,username,password,email,firstname,lastname,ip);
+
+			Log.e("Usuario", user.getUsername() + "");
+		} 
+	}
+
+	private class RegistroUsuarioJSONFeedTask extends
+			AsyncTask<String, Void, String> {		
+		
+		@Override
+		protected void onPreExecute() {
+			Util.MostrarDialog(Login.this);
+			super.onPreExecute();
+		}
+		
+		protected String doInBackground(String... prms) {
+			return Util.readJSONFeedPost(prms[0], prms[1], prms[2], prms[3],
+					prms[4], prms[5],prms[6]);
+		}
+
+		protected void onPostExecute(String result) {
+			try {
+				JSONObject datos = new JSONObject(result);
+				if (result.equals("error")) {
+					Toast.makeText(getApplicationContext(),
+							"Error al conectar con el servidor",
+							Toast.LENGTH_SHORT).show();
+				} else if (datos.getString("res").equalsIgnoreCase("ok")) {
+					SharedPreferences prefe = getSharedPreferences("user",
+							Context.MODE_PRIVATE);
+					Editor editor = prefe.edit();
+					editor.putString("id", datos.getString("data"));
+					editor.commit();
+
+					Session.getActiveSession().closeAndClearTokenInformation();				
+					Intent i = new Intent(getApplicationContext(),
+							MainActivity.class);
+					startActivity(i);					
+					finish();
+				} else if (datos.getString("res").equalsIgnoreCase("error")) {
+					Toast.makeText(getApplicationContext(), "Error interno",
+							Toast.LENGTH_SHORT).show();
+				}
+			} catch (Exception e) {
+				Log.e("Error onPostExecute", "No se descargaron los datos");
+			}
+		}	
+		
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	private void handlePendingAction() {
+		PendingAction previouslyPendingAction = pendingAction;		
+		pendingAction = PendingAction.NONE;
+
+		switch (previouslyPendingAction) {
+		case POST_PHOTO:
+			break;
+		case POST_STATUS_UPDATE:
+			postStatusUpdate();
+			break;
+		}
+	}
+
+	private interface GraphObjectWithId extends GraphObject {
+		String getId();
+	}
+
+	private void showPublishResult(String message, GraphObject result,
+			FacebookRequestError error) {
+		String title = null;
+		String alertMessage = null;
+		if (error == null) {
+			title = getString(R.string.success);
+			String id = result.cast(GraphObjectWithId.class).getId();
+			alertMessage = getString(R.string.successfully_posted_post,
+					message, id);
+		} else {
+			title = getString(R.string.error);
+			alertMessage = error.getErrorMessage();
+		}
+
+		new AlertDialog.Builder(this).setTitle(title).setMessage(alertMessage)
+				.setPositiveButton(R.string.ok, null).show();
+	}
+
+	private FacebookDialog.ShareDialogBuilder createShareDialogBuilderForLink() {
+		return new FacebookDialog.ShareDialogBuilder(this)
+				.setName("Hello Facebook")
+				.setDescription(
+						"The 'Hello Facebook' sample application showcases simple Facebook integration")
+				.setLink("http://developers.facebook.com/android");
+	}
+
+	private void postStatusUpdate() {
+		if (canPresentShareDialog) {
+			FacebookDialog shareDialog = createShareDialogBuilderForLink()
+					.build();
+			uiHelper.trackPendingDialogCall(shareDialog.present());
+		} else if (user != null && hasPublishPermission()) {
+			final String message = getString(R.string.status_update,
+					user.getFirstName(), (new Date().toString()));
+			Request request = Request.newStatusUpdateRequest(
+					Session.getActiveSession(), message, place, tags,
+					new Request.Callback() {
+						@Override
+						public void onCompleted(Response response) {
+							showPublishResult(message,
+									response.getGraphObject(),
+									response.getError());
+						}
+					});
+			request.executeAsync();
+		} else {
+			pendingAction = PendingAction.POST_STATUS_UPDATE;
+		}
+	}
+
+	private void setFriendPickerListeners(final FriendPickerFragment fragment) {
+		fragment.setOnDoneButtonClickedListener(new FriendPickerFragment.OnDoneButtonClickedListener() {
+			@Override
+			public void onDoneButtonClicked(PickerFragment<?> pickerFragment) {
+				
+			}
+		});
+	}
+
+	/*private void onPlacePickerDone(PlacePickerFragment fragment) {
+		FragmentManager fm = getSupportFragmentManager();
+		fm.popBackStack();
+		String result = "";
+		GraphPlace selection = fragment.getSelection();
+		if (selection != null) {
+			result = selection.getName();
+		} else {
+			result = getString(R.string.no_place_selected);
+		}
+		place = selection;
+		showAlert(getString(R.string.you_picked), result);
+	}*/
+
+	private void setPlacePickerListeners(final PlacePickerFragment fragment) {
+		fragment.setOnDoneButtonClickedListener(new PlacePickerFragment.OnDoneButtonClickedListener() {
+			@Override
+			public void onDoneButtonClicked(PickerFragment<?> pickerFragment) {
+				//onPlacePickerDone(fragment);
+			}
+		});
+		fragment.setOnSelectionChangedListener(new PlacePickerFragment.OnSelectionChangedListener() {
+			@Override
+			public void onSelectionChanged(PickerFragment<?> pickerFragment) {
+				if (fragment.getSelection() != null) {
+					//onPlacePickerDone(fragment);
+				}
+			}
+		});
+	}
+
+	private void showAlert(String title, String message) {
+		new AlertDialog.Builder(this).setTitle(title).setMessage(message)
+				.setPositiveButton(R.string.ok, null).show();
+	}
+
+	private boolean hasPublishPermission() {
+		Session session = Session.getActiveSession();
+		return session != null
+				&& session.getPermissions().contains("publish_actions");
 	}
 }
